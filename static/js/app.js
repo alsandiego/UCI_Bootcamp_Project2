b1e8b657c197d5ccac403a451c152af169fbe27c
Al San Diego <al.sandiego@Al-San-Diegos-Work-MacBook-Pro.local>
2020-01-03 16:42:47 -0800

    updated sqlite database

---
+++ static/js/app.js
@@ -0,0 +1,17 @@
+// function init() {
+//     // Grab a reference to the dropdown select element
+//     var selector = d3.select("#selDataset");
+  
+//     // Use the list of sample names to populate the select options
+//     d3.json("/year").then((listYear) => {
+//       listYear.forEach((year) => {
+//         selector
+//           .append("option")
+//           .text(year)
+//           .property("value", year);
+//       });
+//     });
+//   }
+  
+//   // Initialize the dashboard
+//   init();
