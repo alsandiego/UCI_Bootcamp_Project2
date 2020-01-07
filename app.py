4a334e6dd71b6efd0d49cc60b252afe57f482d6d
Al San Diego <al.sandiego@dhcp-v093-065.mobile.uci.edu>
2020-01-06 21:40:20 -0800

    working app.route

--- app.py
+++ app.py
@@ -44,16 +44,56 @@ def year():
     # Return a list of the column names (sample names)
     return jsonify(list(df.columns)[3:])
 
+# shows salary and position for selected year and department
+@app.route("/<yr>/<dept>")
+def year_dept(yr, dept):
+    """Return department salary for year selected."""
+    # Use Pandas to perform the sql query
+    stmt = db.session.query(oc_salary_db).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
+    # print(dept.upper())
+    dept_total = df.loc[(df[yr] >0) & (df["department"] == dept.upper()), [dept, "position", yr]]
+    # Format the data to send as json
+    data = {
+        "department": dept_total[dept].values.tolist(),
+        "salary": dept_total[yr].values.tolist(),
+        "position": dept_total.position.tolist()
+        
+    }
+    return jsonify(data)
+
 @app.route("/department")
 def department():
     """Return a list of departments."""
-    
     # Use Pandas to perform the sql query
     stmt = db.session.query(oc_salary_db).statement
     df = pd.read_sql_query("select department from oc_salary group by department", db.session.bind, coerce_float=False)
 
     # Return a list of the column names (sample names)
     return jsonify(list(df["department"].values))
+
+@app.route("/department/<year>")
+def dept(year):
+    """Return department, position."""
+    stmt = db.session.query(oc_salary_db).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
+
+    # Filter the data based on the sample number and
+    # only keep rows with values above 1
+
+    # sample_data = df.loc[(df[year] >0) & (df["department"]== "ACCOUNTING"), ["department", "position", year]]
+
+    sample_data = df.loc[(df[year] >0), ["position", year]]
+
+    # Sort by sample
+    sample_data.sort_values(by="position", ascending=True, inplace=True)
+
+    # Format the data to send as json
+    data = {
+        "position": sample_data.position.tolist(),
+        "salary": sample_data[year].values.tolist()
+    }
+    return jsonify(data)
 
 @app.route("/position/<year>")
 def position(year):
@@ -63,7 +103,10 @@ def position(year):
 
     # Filter the data based on the sample number and
     # only keep rows with values above 1
-    sample_data = df.loc[(df[year] >0) & (df["department"]== "ACCOUNTING"), ["department", "position", year]]
+
+    # sample_data = df.loc[(df[year] >0) & (df["department"]== "ACCOUNTING"), ["department", "position", year]]
+
+    sample_data = df.loc[(df[year] >0), ["department", "position", year]]
 
     # Sort by sample
     sample_data.sort_values(by="department", ascending=True, inplace=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,50 @@
+function buildMetadata(year) {
+
+  // @TODO: Complete the following function that builds the metadata panel
+
+  // Use `d3.json` to fetch the metadata for a sample
+  var url = `/position/${year}`;
+  d3.json(url).then(function(response){
+    // Use d3 to select the panel with id of `#sample-metadata`
+    var sample_meta = d3.select("#sample-metadata");
+    // Use `.html("") to clear any existing metadata
+    sample_meta.html("");
+    // Use `Object.entries` to add each key and value pair to the panel
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // tags for each key-value in the metadata.
+    Object.entries(response).forEach(([key, value]) => {
+      sample_meta.append("p").text(`${key}: ${value}`);
+    });
+    console.log(sample_meta)
+  });
+    // BONUS: Build the Gauge Chart
+    // buildGauge(data.WFREQ);
+};
+
+// function buildMetadata(year) {
+
+//   // @TODO: Complete the following function that builds the metadata panel
+
+//   // Use `d3.json` to fetch the metadata for a sample
+//   var url = `/${department}/${year}`;
+//   d3.json(url).then(function(response){
+//     // Use d3 to select the panel with id of `#sample-metadata`
+//     var sample_meta = d3.select("#sample-metadata");
+//     // Use `.html("") to clear any existing metadata
+//     sample_meta.html("");
+//     // Use `Object.entries` to add each key and value pair to the panel
+//     // Hint: Inside the loop, you will need to use d3 to append new
+//     // tags for each key-value in the metadata.
+//     Object.entries(response).forEach(([key, value]) => {
+//       sample_meta.append("p").text(`${key}: ${value}`);
+//     });
+//     console.log(sample_meta)
+//   });
+//     // BONUS: Build the Gauge Chart
+//     // buildGauge(data.WFREQ);
+// };
+
+
 function init() {
     // Grab a reference to the dropdown select element
     var selector = d3.select("#selDataset");
@@ -10,8 +57,17 @@ function init() {
           .text(year)
           .property("value", year);
       });
+
+    // Use the first sample from the list to build the initial plots
+      const firstData = listYear[0];
+      buildMetadata(firstData);
     });
   }
   
+  function optionChanged(newSample) {
+    // Fetch new data each time a new sample is selected
+    buildMetadata(newSample);
+  }
+
   // Initialize the dashboard
   init();
