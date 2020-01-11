16e524df5153f660675927dea266cd9274d83ac4
Al San Diego <al.sandiego@dhcp-v107-137.mobile.uci.edu>
2020-01-11 11:53:45 -0800

    beta 2

--- static/js/app.js
+++ static/js/app.js
@@ -1,27 +1,3 @@
-// function buildMetadata(year) {
-
-//   // @TODO: Complete the following function that builds the metadata panel
-
-//   // Use `d3.json` to fetch the metadata for a sample
-//   var url = `/year/${year}`;
-//   d3.json(url).then(function(response){
-//     // Use d3 to select the panel with id of `#sample-metadata`
-//     var sample_meta = d3.select("#sample-metadata");
-//     // Use `.html("") to clear any existing metadata
-//     sample_meta.html("");
-//     // Use `Object.entries` to add each key and value pair to the panel
-//     // Hint: Inside the loop, you will need to use d3 to append new
-//     // tags for each key-value in the metadata.
-//     Object.entries(response).forEach(([key, value]) => {
-//       sample_meta.append("p").text(`${key}: ${value}`);
-//     });
-//     console.log(sample_meta)
-//   });
-//     // BONUS: Build the Gauge Chart
-//     // buildGauge(data.WFREQ);
-// };
-
-
 ////////////////////////////
 // PAGE 1 - VIEW BY YEAR //
 ///////////////////////////
@@ -132,154 +108,7 @@ function init() {
       buildCharts(firstData);
       buildCanvas(firstData);
     });
-  }
-  
-function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
-  clearBubble()
-  buildCharts(newSample);
-  buildCharts2(newSample);
-  buildCanvas(newSample);
-  // buildMetadata(newSample);
-}
-
-// Initialize the dashboard
-init();
-
-////////////////////////////
-// PAGE 2 - VIEW BY DEPT //
-///////////////////////////
-
-function buildCharts2(department) {
-
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
-  var scatterURL =  `/bubble2/${department}`;
-  d3.json(scatterURL).then(function(data){
-    // our data dict
-    var position = data.position
-    var salary2014 = data["2014"]
-    var salary2015 = data["2015"]
-    var salary2016 = data["2016"]
-    var salary2017 = data["2017"]
-    var salary2018 = data["2018"]
-
-    var trace1 = {
-      x: position,
-      y: salary2014,
-      // mode: 'markers',
-      type: 'bar',
-      name: "2014"
-    };
-    
-    var trace2 = {
-      x: position,
-      y: salary2015,
-      // mode: 'markers',
-      type: 'bar',
-      name: "2015"
-    };
-
-    var trace3 = {
-      x: position,
-      y: salary2016,
-      // mode: 'markers',
-      type: 'bar',
-      name: "2016"
-    };
-    
-    var trace4 = {
-      x: position,
-      y: salary2017,
-      // mode: 'markers',
-      type: 'bar',
-      name: "2017"
-    };
-
-    var trace5 = {
-      x: position,
-      y: salary2018,
-      // mode: 'markers',
-      type: 'bar',
-      name: "2018"
-    };
-    
-    var data = [trace1, trace2, trace3, trace4, trace5];
-    var layout = {barmode: 'group'}
-    scatter = document.getElementById("scatter");
-    Plotly.newPlot("scatter", data, layout, {responsive: true})
-
-
-    //////////// to show department table
-    var dept_values = [position,salary2014,salary2015,salary2016,salary2017,salary2018]
-    var dept_table = [{
-    type: 'table',
-    header: {
-      values: [["<b>Position</b>"], ["<b>2014</b>"],["<b>2015</b>"],["<b>2016</b>"],["<b>2017</b>"],["<b>2018</b>"]],
-      align: "center",
-      line: {width: 1, color: 'black'},
-      fill: {color: "grey"},
-      font: {family: "Arial", size: 12, color: "white"}
-    },
-    cells: {
-      values: dept_values,
-      align: "center",
-      line: {color: "black", width: 1},
-      font: {family: "Arial", size: 11, color: ["black"]}
-    }
-    }]
-    table = document.getElementById("table2");
-    Plotly.newPlot(table, dept_table);
-
-  }); 
-};
-
-function init2() {
-  // Grab a reference to the dropdown select element
-    var selector = d3.select("#selDataset2");
-  // Use the list of sample names to populate the select options
-    d3.json("/department").then((listDepartment) => {
-      listDepartment.forEach((department) => {
-        selector
-          .append("option")
-          .text(department)
-          .property("value", department);
-      });
-
-  // Use the first sample from the list to build the initial plots
-      const firstData2 = listDepartment[0];
-      buildCharts2(firstData2);
-    });
-  }
-
-init2();
-
-//   function optionChanged(newDept) {
-//   // Fetch new data each time a new sample is selected
-//   buildCharts(newDept);
-//   // buildMetadata(newSample);
-// };
-
-
-    // @TODO: Build a Bubble Chart using the sample data
-    // var trace1 = {
-    //   x: department,
-    //   y: total_salary,
-    //   text: department,
-    //   mode: 'markers',
-    //   marker: {
-    //   size: total_salary,
-    //   color: department
-    //   }
-    // };
-    
-    // var data_bubble = [trace1];
-    
-    // var layout = {
-    //   xaxis: { title: 'Departments'}
-    // };
-    
-    // bubble = document.getElementById("bubble");
-    // Plotly.newPlot(bubble, data_bubble, layout);
+  } 
 
 var myChart;
 
@@ -349,3 +178,14 @@ function clearBubble()
   myChart.destroy()
   document.getElementById("bubble").innerHTML = "";
 }
+
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  clearBubble()
+  buildCharts(newSample);
+  buildCanvas(newSample);
+  // buildMetadata(newSample);
+}
+
+// Initialize the dashboard
+init();
