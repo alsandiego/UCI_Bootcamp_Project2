40dc63324d7ab25aea95e50671f019b4fd61edb7
Al San Diego <al.sandiego@Al-San-Diegos-Work-MacBook-Pro.local>
2020-01-13 15:17:58 -0800

    final version

--- static/js/app2.js
+++ static/js/app2.js
@@ -59,7 +59,7 @@ function buildCharts2(department) {
     var layout = {barmode: 'group',
                     margin: {l: 50,
                               r: 50,
-                              b: 50,
+                              b: 100,
                               t: 50,
                               pad: 4}}
     scatter = document.getElementById("scatter");
@@ -176,6 +176,8 @@ function init2() {
   // Use the first sample from the list to build the initial plots
       const firstData2 = listDepartment[0];
       buildCharts2(firstData2);
+      pieTotalSalary(firstData2);
+      top10position(firstData2);
     });
   }
 
@@ -185,5 +187,93 @@ init2();
 function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
   buildCharts2(newSample);
+  pieTotalSalary(newSample);
+  top10position(newSample);
   // buildMetadata(newSample);
+}
+
+function pieTotalSalary(department) {
+  // @TODO: Use `d3.json` to fetch the sample data for the plots
+  var scatterURL =  `/bubble2/${department}`;
+  d3.json(scatterURL).then(function(data){
+    // our data dict
+    var position = data.position
+    var salary2014 = data["2014"]
+    var salary2015 = data["2015"]
+    var salary2016 = data["2016"]
+    var salary2017 = data["2017"]
+    var salary2018 = data["2018"]
+    var sumsalary = salary2014+salary2015+salary2016+salary2017+salary2018
+
+    var data_pie = [{
+      type: "pie",
+      values: sumsalary,
+      labels: position,
+      textinfo: "label+percent",
+      textposition: "inside",
+      showlegend: false
+    }]
+
+    var layout_pie = {
+      autosize: true,
+      // height: 800,
+      // width: 1080,
+      title: {
+        text:'Total Salary per Position',
+        font: {
+          family: 'Courier New, monospace',
+          size: 24
+        },
+      margin: {"t": 0, "b": 0, "l": 0, "r": 0}
+      }
+    };
+
+    pie = document.getElementById("pieTotalSalary");
+    Plotly.newPlot(pie, data_pie, layout_pie, { responsive: true });
+})
+}
+
+function top10position(department) {
+  var scatterURL =  `/bubble2/${department}`;
+  d3.json(scatterURL).then(function(data){
+    // our data dict
+    var dict = [];
+    for (var c = 0; c < data.position.length; c++) {
+      dict.push({
+          "position": data.position[c],
+          "total_salary": data["2014"][c]+data["2015"][c]+data["2016"][c]+data["2017"][c]+data["2018"][c]
+      });
+    }
+    dict.sort(function(a, b) {
+      return parseFloat(b.total_salary) - parseFloat(a.total_salary);
+    });
+    dict = dict.slice(0, 10);
+
+    var data_pie = [{
+      type: "pie",
+      values: dict.map(row => row.total_salary),
+      labels: dict.map(row => row.position),
+      textinfo: "label+percent",
+      textposition: "inside",
+      showlegend: false
+    }]
+
+    var layout_pie = {
+      autosize: true,
+      // height: 800,
+      // width: 1080,
+      title: {
+        text:'Top 10 Positions',
+        font: {
+          family: 'Courier New, monospace',
+          size: 24
+        },
+      margin: {"t": 0, "b": 0, "l": 0, "r": 0}
+      }
+    };
+
+    pie = document.getElementById("deptTop10");
+    Plotly.newPlot(pie, data_pie, layout_pie, { responsive: true });
+
+})
 }
